37fcf87290bc47229596b39710f917b6096a7a15
Mike Gainey <31196587+mikegainey@users.noreply.github.com>
2017-09-10 22:59:54 -0500

    deleted output added by mistake

--- homework_01.py
+++ homework_01.py
@@ -22,23 +22,3 @@ t = int(t_input)  # number of years
 
 A = P * (1 + (r / n))**(n * t)  # compute compound interest
 print("The amount due when years =", t, "is", A)
-
-
-
-
-Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
-[GCC 5.4.0 20160609] on linux
-Type "copyright", "credits" or "license()" for more information.
->>> 
-========== RESTART: /home/michael/prog/git/CSCI1470/homework_01.py ==========
-How many years? 10
-The amount due when years = 10 is 22196.40234544711
->>> 
-========== RESTART: /home/michael/prog/git/CSCI1470/homework_01.py ==========
-How many years? 25
-The amount due when years = 25 is 73401.75963739285
->>> 
-========== RESTART: /home/michael/prog/git/CSCI1470/homework_01.py ==========
-How many years? 50
-The amount due when years = 50 is 538781.8317865595
->>> 

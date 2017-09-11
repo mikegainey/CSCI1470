429e1b472c47bfc937242e889c9ad1e6599ebdfc
mikegainey <mikeg9b@gmail.com>
2017-09-10 22:36:51 -0500

    added the header template for homework assignments

---
+++ homework_header.py
@@ -0,0 +1,15 @@
+#**********************  orAndTest.py  *********************
+#
+# Name: Patrick Rothfuss
+#
+# Course: CSCI 1470
+#
+# Assignment: Homework #6
+#
+# Algorithm:
+#   Declarations
+#   Prompt user for dayOfWeek
+#   Get dayOfWeek
+#   if dayOfWeek is Tuesday
+#      ...
+#**********************************************************

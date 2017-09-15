528bbb0bcb61a2708b6106de8f0fc45ad28d7669
Michael Gainey <mikeg9b@gmail.com>
2017-09-15 15:23:53 -0500

    fractional ages round down; fractional hours are kept

--- homework-03.py
+++ homework-03.py
@@ -11,7 +11,7 @@
 #     Assign variables with user input
 #     Using age, determine the hourly fee
 #     Compute the bill (hours * hourly fee)
-#     Print the final bill with all relavent information
+#     Print the final bill with all relevant information
 #   
 ############################################################
 
@@ -20,21 +20,23 @@ for child in range(3): # loops for each child: 0, 1, and 2
     print()
     print("=" * 80) # horizontal line
     print("Welcome to UH Clear Lake Child Care!")
-
+    print("=" * 80) # horizontal line
+    
     # Assign variables with user input
     fname = input("  What is your child's first name? ")
     lname = input("  What is the family name? ")
 
     while True:
         age = input("  What is your child's age? ")
-        age = int(age)
+        age = float(age) # this converts any numeric string (like "3.5") to a float
+        age = int(age)   # this rounds any float down (so an age of 3.5 is considered a 3-year-old)
         if (age > 10) or (age < 0):
             print("    (We only accept children through age 10.)")
         else:
-            break # age is <= 10, so continue the program (otherwise, ask again)
+            break # age is within range, so continue the program (otherwise, ask again)
 
     hours = input("  How many hours will your child stay today? ")
-    hours = int(hours)
+    hours = float(hours) # hours doesn't have to be a whole number
 
     # Using age, determine the hourly fee
     #   Note: "... expressions like a < b < c have the interpretation that is conventional in mathematics"
@@ -55,6 +57,7 @@ for child in range(3): # loops for each child: 0, 1, and 2
 
     # Display the final bill with all relavent information
     print()
-    print("{} {}, age {}, stayed {} hours at ${} per hour. You owe ${} today.".format(fname, lname, age, hours, fee, bill))
+    print("{} {}, age {}, stayed {} hours at ${} per hour. You owe ${:.2f} today."\
+          .format(fname, lname, age, hours, fee, bill))
     print("=" * 80) # horizontal line
     print()

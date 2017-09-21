e734e2f6515bf67b7708c1bb157028a94ed7b550
Michael Gainey <mikeg9b@gmail.com>
2017-09-20 20:08:11 -0500

    actually followed the instructions completely
    asks user for item cost AND amount tendered

--- day6-change.py
+++ day6-change.py
@@ -5,11 +5,16 @@
 
 while True:
     print()
+    cost = float(input("Enter the cost of the item (up to $5):" ))
     tendered = float(input("Enter the amount tendered: "))
-    if tendered < 0:
+    if cost < 0 or tendered < 0:
         print("  This program only works with positive number inputs.")
     else:
         break # the amount tendered is in range; continue the program
+
+change = tendered - cost
+change = round(change, 2) # 30 - 23.46 = 6.539999999999999 (in Python); round to 6.54
+tempchange = change
 
 fives = 0
 ones = 0
@@ -18,24 +23,24 @@ dimes = 0
 nickles = 0
 pennies = 0
 
-fives = int(tendered // 5)
-tendered %= 5
+fives = int(tempchange // 5) # floor division returns a float! Why?
+tempchange %= 5
 
-ones = int(tendered // 1)
-tendered %= 1
+ones = int(tempchange // 1)
+tempchange %= 1
 
-quarters = int(tendered // .25)
-tendered %= .25
+quarters = int(tempchange // .25)
+tempchange %= .25
 
-dimes = int(tendered // .1)
-tendered %= .1
+dimes = int(tempchange // .1)
+tempchange %= .1
 
-nickles = int(tendered // .05)
-tendered %= .05
+nickles = int(tempchange // .05)
+tempchange %= .05
 
-pennies = int(tendered // .01)
-tendered %= .01
+pennies = int(tempchange // .01)
+tempchange %= .01
 
-print("That is {} fives, {} ones, {} quarters, {} dimes, {} nickles, and {} pennies."\
-      .format(fives, ones, quarters, dimes, nickles, pennies))
+print("Your change is {} ({} fives, {} ones, {} quarters, {} dimes, {} nickles, and {} pennies)."
+      .format(change, fives, ones, quarters, dimes, nickles, pennies))
 print()

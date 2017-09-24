620d9f3a184f6a4b2d66fbacdbe493e9a801beb8
Michael Gainey <mikeg9b@gmail.com>
2017-09-23 19:02:31 -0500

    changed the while-loop to include an lcv and conditional expression
    also improved spacing of the flowchart

--- GaineyM_CSCI1470_HW4.py
+++ GaineyM_CSCI1470_HW4.py
@@ -9,44 +9,45 @@
 #
 # Pseudocode:
 #
-# Start a while loop
+#   Set another to 'y' so the loop will execute
+#   Loop while another equals 'y'
 #
-#   Prompt the user to enter the cost of an item
-#   Prompt the user to enter the amount tendered
-#   If the amount tendered is not sufficient, go to the top of the loop
-#  
-#   Compute the change (tendered - cost)
-#   Set tempchange to also equal change
-#  
-#   Initialize variables for monetary denominations (ones, quarters, dimes, nickles, and pennies) to zero
-#  
-#   Set ones to the number of times 1 will divide evenly into change
-#   Set tempchange to whatever amount is left
-#  
-#   Set quarters to the number of times .25 will divide evenly into change
-#   Set tempchange to whatever amount is left
+#     Prompt the user to enter the cost of an item
+#     Prompt the user to enter the amount tendered
+#     If the amount tendered is not sufficient to cover the cost, go to the top of the loop
 #
-#   Set dimes to the number of times .10 will divide evenly into change
-#   Set tempchange to whatever amount is left
+#     Compute the change (tendered - cost)
+#     Set tempchange to also equal change
 #
-#   Set nickles to the number of times .05 will divide evenly into change
-#   Set tempchange to whatever amount is left
+#     Initialize variables for monetary denominations (ones, quarters, dimes, nickles, and pennies) to zero
 #
-#   Set pennies to the number of times .01 will divide evenly into change
-#  
-#   Print the amount of change including the number of one dollar bills, quarters, dimes, nickles, and pennies
-#  
-#   Prompt user to buy another item (default is no)
-#   Set another to 'n' if the user just presses <Enter>
-#   Set another to equal only its first character and convert to lowercase
-#  
-#   If another is 'y' go to the top of the loop, otherwise the program ends
+#     Set ones to the number of times 1 will divide evenly into change
+#     Set tempchange to whatever amount is left (the remainder)
+#
+#     Set quarters to the number of times .25 will divide evenly into change
+#     Set tempchange to whatever amount is left
+#
+#     Set dimes to the number of times .10 will divide evenly into change
+#     Set tempchange to whatever amount is left
+#
+#     Set nickles to the number of times .05 will divide evenly into change
+#     Set tempchange to whatever amount is left
+#
+#     Set pennies to the number of times .01 will divide evenly into change
+#
+#     Print the amount of change including the number of one dollar bills, quarters, dimes, nickles, and pennies
+#
+#     Prompt user to buy another item (default is no)
+#     Set another to 'n' if the user just presses <Enter>
+#     Set another to equal only its first character and convert it to lowercase
+#
+#     If another is 'y' the program will loop, otherwise the program ends
 #
 ############################################################
 
+another = 'y' # At the end, the user will be asked if they want to enter "another" item
 
-while True: # this is how a do-while loop is done in Python; the break is at the bottom of the program
-            # see https://www.python.org/dev/peps/pep-0315/
+while another == 'y':
 
     # Ask the user to enter an item cost and an amount tendered.
     print()
@@ -54,14 +55,14 @@ while True: # this is how a do-while loop is done in Python; the break is at the
     tendered = float(input("  Enter the amount tendered: "))
 
     # Check to make sure that the amount tendered is sufficient to cover the cost of the item;
-    # if not, print an error message and start over
+    # If not, print an error message and go back to the top of the loop
     if tendered < cost:
         print("Error: You need to pay an amount sufficient to cover the cost.")
         continue # go back to the top of the while-loop
 
     # compute the amount of change due
     change = tendered - cost # change won't change
-    tempchange = change      # tempchange will change (mutate)
+    tempchange = change      # tempchange will mutate
 
     # initialize some variables
     ones, quarters, dimes, nickles, pennies = 0, 0, 0, 0, 0
@@ -80,18 +81,16 @@ while True: # this is how a do-while loop is done in Python; the break is at the
     tempchange = round(tempchange % .05, 2)
 
     pennies = int(tempchange / .01)
-    
+
     # Report the amount of change given: number of dollar bills, quarters, dimes, nickels and pennies.
-    print("Your change is ${:.2f}. That's {} ones, {} quarters, {} dimes, {} nickles, and {} pennies."
+    print("Your change is ${:.2f}.  That's {} ones, {} quarters, {} dimes, {} nickles, and {} pennies."
           .format(change, ones, quarters, dimes, nickles, pennies))
 
-    # Buy another item?
-    another = input("Do you want to buy another item? (y/N) ") # 'no' is the defalt choice
+    # Buy another item? This sets up the lcv to either loop again or not.
+    another = input("  Do you want to buy another item? (y/N) ")
     if another == '': # just pressing <Enter> = no
         another = 'n'
     another = another[0].lower() # keep just the first character and convert to lowercase
-    if another == 'y':
-        continue # go back to the top of the while-loop
-    else:
-        print()
-        break # break out of the loop and end the program
+    # if another == 'y' the program will run again, otherwise it will end
+
+print() # put a space before the next prompt

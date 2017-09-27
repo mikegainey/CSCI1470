7228847ec6e56bd6cb01665ef58f5cc1f4b20a42
Mike Gainey <mikeg9b@gmail.com>
2017-09-26 23:13:29 -0500

    reworked error check and put most of the program inside the if-clause
    if amount tendered >= cost, change computation executes within the if-clause
    otherwise, print the error (else-clause) and ask for another purchase
    
    also, included an output screenshot
    
    also, renamed flowchart files (but they don't match the program logic anymore!)

--- GaineyM_CSCI1470_HW4.py
+++ GaineyM_CSCI1470_HW4.py
@@ -16,29 +16,30 @@
 #
 #     Prompt the user to enter the cost of an item.
 #     Prompt the user to enter the amount tendered.
-#     If the amount tendered is not sufficient to cover the cost, print an error message.
-#     and continue to the top of the loop.
+#     If the amount tendered is sufficient to cover the cost of the item, compute the change ...
 #
-#     Compute the change (tendered - cost).
-#     Set tempchange to also equal change. tempchange will hold the running remainder.
+#       Compute the change (tendered - cost).
+#       Set tempchange to also equal change. tempchange will hold the running remainder.
 #
-#     Initialize variables for monetary denominations (ones, quarters, dimes, nickles, and pennies) to zero.
+#       Initialize variables for monetary denominations (ones, quarters, dimes, nickles, and pennies) to zero.
 #
-#     Set ones to the number of times 1 will divide evenly into tempchange.
-#     Set tempchange to whatever amount is left (the remainder).
+#       Set ones to the number of times 1 will divide evenly into tempchange.
+#       Set tempchange to whatever amount is left (the remainder).
 #
-#     Set quarters to the number of times .25 will divide evenly into tempchange.
-#     Set tempchange to whatever amount is left.
+#       Set quarters to the number of times .25 will divide evenly into tempchange.
+#       Set tempchange to whatever amount is left.
 #
-#     Set dimes to the number of times .10 will divide evenly into tempchange.
-#     Set tempchange to whatever amount is left.
+#       Set dimes to the number of times .10 will divide evenly into tempchange.
+#       Set tempchange to whatever amount is left.
 #
-#     Set nickles to the number of times .05 will divide evenly into tempchange.
-#     Set tempchange to whatever amount is left.
+#       Set nickles to the number of times .05 will divide evenly into tempchange.
+#       Set tempchange to whatever amount is left.
 #
-#     Set pennies to the number of times .01 will divide evenly into tempchange.
+#       Set pennies to the number of times .01 will divide evenly into tempchange.
 #
-#     Print the amount of change including the number of one dollar bills, quarters, dimes, nickles, and pennies.
+#       Print the amount of change including the number of one dollar bills, quarters, dimes, nickles, and pennies.
+#
+#     Otherwise (if the amount tendered is not sufficient), print an error message
 #
 #     Prompt the user to buy another item (default is no).
 #     Set another to 'n' if the user just presses <Enter>.
@@ -54,40 +55,41 @@ while another == 'y':
 
     # Ask the user to enter an item cost and an amount tendered.
     print()
-    cost = float(input("  Enter the cost of the item (up to $5): " ))
+    cost = float(input("  Enter the cost of the item: " ))
     tendered = float(input("  Enter the amount tendered: "))
 
     # Check to make sure that the amount tendered is sufficient to cover the cost of the item;
-    # If not, print an error message and go back to the top of the loop
-    if tendered < cost:
+    # If not, print an error message
+    if tendered >= cost:
+
+        # compute the amount of change due
+        change = tendered - cost # change won't change
+        tempchange = change      # tempchange will hold the running remainder
+
+        # initialize some variables
+        ones, quarters, dimes, nickles, pennies = 0, 0, 0, 0, 0
+
+        # compute the number of ones, quarters, dimes, nickles, and pennies
+        ones = int(tempchange / 1)
+        tempchange = round(tempchange % 1, 2)
+
+        quarters = int(tempchange / .25)
+        tempchange = round(tempchange % .25, 2)
+
+        dimes = int(tempchange / .1)
+        tempchange = round(tempchange % .10, 2)
+
+        nickles = int(tempchange / .05)
+        tempchange = round(tempchange % .05, 2)
+
+        pennies = int(tempchange / .01)
+
+        # Report the amount of change given: number of dollar bills, quarters, dimes, nickels and pennies.
+        print("Your change is ${:.2f}.  That's {} ones, {} quarters, {} dimes, {} nickles, and {} pennies."
+              .format(change, ones, quarters, dimes, nickles, pennies))
+
+    else: # the amount tendered was not sufficient to cover the cost of the item
         print("Error: You need to pay an amount sufficient to cover the cost.")
-        continue # go back to the top of the while-loop
-
-    # compute the amount of change due
-    change = tendered - cost # change won't change
-    tempchange = change      # tempchange will hold the running remainder
-
-    # initialize some variables
-    ones, quarters, dimes, nickles, pennies = 0, 0, 0, 0, 0
-
-    # compute the number of ones, quarters, dimes, nickles, and pennies
-    ones = int(tempchange / 1)
-    tempchange = round(tempchange % 1, 2)
-
-    quarters = int(tempchange / .25)
-    tempchange = round(tempchange % .25, 2)
-
-    dimes = int(tempchange / .1)
-    tempchange = round(tempchange % .10, 2)
-
-    nickles = int(tempchange / .05)
-    tempchange = round(tempchange % .05, 2)
-
-    pennies = int(tempchange / .01)
-
-    # Report the amount of change given: number of dollar bills, quarters, dimes, nickels and pennies.
-    print("Your change is ${:.2f}.  That's {} ones, {} quarters, {} dimes, {} nickles, and {} pennies."
-          .format(change, ones, quarters, dimes, nickles, pennies))
 
     # Buy another item? This sets up the lcv to either loop again or not.
     another = input("  Do you want to buy another item? (y/N) ")

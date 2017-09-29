9728beb427282757ac1a115fe06fb9277f34d7bb
Michael Gainey <mikeg9b@gmail.com>
2017-09-29 17:36:51 -0500

    added the pseudocode in the preface

--- GaineyM_CSCI1470_HW5.py
+++ GaineyM_CSCI1470_HW5.py
@@ -7,27 +7,84 @@
 # Assignment: Homework #5
 #
 # Algorithm:
-#   blah, blah, blah
-#   
+#
+#   Import the random module in order to generate random numbers later
+#   Seed the random number generator
+#   Set LOWERBOUND to 1 and UPPERBOUND to 20, the bounds of the numbers used in the quesions
+#   Begin the outer loop (covering the whole quiz)
+#       Initialize (to zero) variables to track the number of questions asked & correct responses
+#       Display the main welcome mesage
+#
+#       Display an "Addition" heading
+#       Begin a loop to cover the 2 addition questions
+#           Generate 2 random numbers (LOWERBOUND to UPPERBOUND), num1 & num2
+#           Display an addition question and get a response
+#           Increment the question counter
+#           Compute the correct answer (num1 + num2)
+#           If the response is correct, tell the student and increment the correct response counter
+#
+#       Display a "Subtraction" heading
+#       Begin a loop to cover the 2 subtraction questions
+#           Generate 2 random numbers (LOWERBOUND to UPPERBOUND), num1 & num2
+#           Display a subtraction question and get a response
+#           Increment the question counter
+#           Compute the correct answer (num1 + num2)
+#           If the response is correct, tell the student and increment the correct response counter
+#
+#       Display a "Multipliction" heading
+#       Begin a loop to cover the 2 multiplication questions
+#           Generate 2 random numbers (LOWERBOUND to UPPERBOUND), num1 & num2
+#           Display a multiplication question and get a response
+#           Increment the question counter
+#           Compute the correct answer (num1 + num2)
+#           If the response is correct, tell the student and increment the correct response counter
+#
+#       Display a "Division" heading with a note to only give the whole number answer 
+#       Begin a loop to cover the 2 division questions
+#           Generate 2 random numbers (LOWERBOUND to UPPERBOUND), num1 & num2
+#           Display a division question and get a response
+#           Increment the question counter
+#           Compute the correct answer (num1 + num2)
+#           If the response is correct, tell the student and increment the correct response counter
+#
+#       Display a "Modulus" heading
+#       Begin a loop to cover the 2 modulus questions
+#           Generate 2 random numbers (LOWERBOUND to UPPERBOUND), num1 & num2
+#           Display a modulus question and get a response
+#           Increment the question counter
+#           Compute the correct answer (num1 + num2)
+#           If the response is correct, tell the student and increment the correct response counter
+#
+#   Display the number of questions answered correctly and the total number of questions
+#
+#   Prompt the user to take the quiz again
+#   if the response is 'y' continue the while-loop
+#   otherwise, break out of the loop and end the program
+#
 ############################################################
 
 import random
+random.seed()
 
-while True:
+LOWERBOUND = 1   # lower bound of the numbers in the questions
+UPPERBOUND = 20  # upper bound of the numbers in the questions
 
-    # used to keep track of number of questions given and number answered correctly
+while True: # this is the logical equivalent of a do-while loop
+            # the loop exit logic is at the end of the program
+
+    # used to keep track of the number of questions given and number answered correctly
     questions, correct = 0, 0
 
     print()
-    print("Test your math skills! Here are 10 math questions.  Good luck!")
+    print("Test your math skills!  Answer these 10 questions.  Good luck!")
     print()
 
-    print("===== Addition! =====")
+    print("===== Addition! =====\n")
 
     for rep in range(2):
 
-        num1 = random.randint(1,20)
-        num2 = random.randint(1,20)
+        num1 = random.randint(LOWERBOUND, UPPERBOUND)
+        num2 = random.randint(LOWERBOUND, UPPERBOUND)
 
         response = int(input("{} + {} = ".format(num1, num2)))
         questions += 1
@@ -40,12 +97,12 @@ while True:
         print()
 
 
-    print("===== Subtraction! =====")
+    print("===== Subtraction! =====\n")
 
     for rep in range(2):
 
-        num1 = random.randint(1,20)
-        num2 = random.randint(1,20)
+        num1 = random.randint(LOWERBOUND, UPPERBOUND)
+        num2 = random.randint(LOWERBOUND, UPPERBOUND)
 
         if num2 > num1:
             num1, num2 = num2, num1
@@ -61,12 +118,12 @@ while True:
         print()
 
 
-    print("===== Multiplication! =====")
+    print("===== Multiplication! =====\n")
 
     for rep in range(2):
 
-        num1 = random.randint(1,20)
-        num2 = random.randint(1,20)
+        num1 = random.randint(LOWERBOUND, UPPERBOUND)
+        num2 = random.randint(LOWERBOUND, UPPERBOUND)
 
         response = int(input("{} x {} = ".format(num1, num2)))
         questions += 1
@@ -79,12 +136,12 @@ while True:
         print()
 
 
-    print("===== Division!  ===== (just the whole number answer)")
+    print("===== Division!  ===== (just the whole number answer)\n")
 
     for rep in range(2):
 
-        num1 = random.randint(1,20)
-        num2 = random.randint(1,20)
+        num1 = random.randint(LOWERBOUND, UPPERBOUND)
+        num2 = random.randint(LOWERBOUND, UPPERBOUND)
 
         if num2 > num1:
             num1, num2 = num2, num1
@@ -100,12 +157,12 @@ while True:
         print()
 
 
-    print("===== Modulus!  ===== (the remainder after division)")
+    print("===== Modulus!  ===== (the remainder after division)\n")
 
     for rep in range(2):
 
-        num1 = random.randint(1,20)
-        num2 = random.randint(1,20)
+        num1 = random.randint(LOWERBOUND, UPPERBOUND)
+        num2 = random.randint(LOWERBOUND, UPPERBOUND)
 
         if num2 > num1:
             num1, num2 = num2, num1
@@ -124,7 +181,8 @@ while True:
     print()
 
     again = input("Do you want to try again? (y/n) ")
-    if again != 'y':
-        break
+    if again == 'y':
+        continue # continue looping
     else:
         print()
+        break    # exit the loop (and end the program)

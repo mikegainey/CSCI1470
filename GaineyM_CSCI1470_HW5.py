63fda0ad657007c66d612a030f61a842265ee221
Michael Gainey <mikeg9b@gmail.com>
2017-09-29 20:01:59 -0500

    changed the while loop to have a lcv
    - put periods at the end of each line of pseudocode
    - removed exclamation points from quiz subheadings
    - added a conditional exclamation point in the final report
      for 80% correct or above
    - allowed pressing <Enter> to mean 'no' at the end
    - allowed 'y' or 'yes' or anything else starting with a 'y' to mean 'yes'

--- GaineyM_CSCI1470_HW5.py
+++ GaineyM_CSCI1470_HW5.py
@@ -8,58 +8,58 @@
 #
 # Algorithm:
 #
-#   Import the random module in order to generate random numbers later
-#   Seed the random number generator
-#   Set LOWERBOUND to 1 and UPPERBOUND to 20, the bounds of the numbers used in the quesions
-#   Begin the outer loop (covering the whole quiz)
-#       Initialize (to zero) variables to track the number of questions asked & correct responses
-#       Display the main welcome mesage
+#   Import the random module in order to generate random numbers later.
+#   Seed the random number generator.
+#   Set LOWERBOUND to 1 and UPPERBOUND to 20, the bounds of the numbers used in the quesions.
+#   Begin the outer loop (covering the whole quiz).
+#       Initialize (to zero) variables to track the number of questions asked & number of correct responses.
+#       Display the main welcome mesage.
 #
-#       Display an "Addition" heading
-#       Begin a loop to cover the 2 addition questions
-#           Generate 2 random numbers (LOWERBOUND to UPPERBOUND), num1 & num2
-#           Display an addition question and get a response
-#           Increment the question counter
-#           Compute the correct answer (num1 + num2)
-#           If the response is correct, tell the student and increment the correct response counter
+#       Display an "Addition" heading.
+#       Begin a loop to cover the 2 addition questions.
+#           Generate 2 random numbers (LOWERBOUND to UPPERBOUND), num1 & num2.
+#           Display an addition question and get a response.
+#           Increment the question counter.
+#           Set answer to the correct answer (num1 + num2).
+#           If the response is correct, tell the student and increment the correct response counter.
 #
-#       Display a "Subtraction" heading
-#       Begin a loop to cover the 2 subtraction questions
-#           Generate 2 random numbers (LOWERBOUND to UPPERBOUND), num1 & num2
-#           Display a subtraction question and get a response
-#           Increment the question counter
-#           Compute the correct answer (num1 + num2)
-#           If the response is correct, tell the student and increment the correct response counter
+#       Display a "Subtraction" heading.
+#       Begin a loop to cover the 2 subtraction questions.
+#           Generate 2 random numbers (LOWERBOUND to UPPERBOUND), num1 & num2.
+#           Display a subtraction question and get a response.
+#           Increment the question counter.
+#           Set answer to the correct answer (num1 - num2).
+#           If the response is correct, tell the student and increment the correct response counter.
 #
-#       Display a "Multipliction" heading
-#       Begin a loop to cover the 2 multiplication questions
-#           Generate 2 random numbers (LOWERBOUND to UPPERBOUND), num1 & num2
-#           Display a multiplication question and get a response
-#           Increment the question counter
-#           Compute the correct answer (num1 + num2)
-#           If the response is correct, tell the student and increment the correct response counter
+#       Display a "Multipliction" heading.
+#       Begin a loop to cover the 2 multiplication questions.
+#           Generate 2 random numbers (LOWERBOUND to UPPERBOUND), num1 & num2.
+#           Display a multiplication question and get a response.
+#           Increment the question counter.
+#           Set answer to the correct answer (num1 * num2).
+#           If the response is correct, tell the student and increment the correct response counter.
 #
-#       Display a "Division" heading with a note to only give the whole number answer 
-#       Begin a loop to cover the 2 division questions
-#           Generate 2 random numbers (LOWERBOUND to UPPERBOUND), num1 & num2
-#           Display a division question and get a response
-#           Increment the question counter
-#           Compute the correct answer (num1 + num2)
-#           If the response is correct, tell the student and increment the correct response counter
+#       Display a "Division" heading (with a note to only give the whole number answer).
+#       Begin a loop to cover the 2 division questions.
+#           Generate 2 random numbers (LOWERBOUND to UPPERBOUND), num1 & num2.
+#           Display a division question and get a response.
+#           Increment the question counter.
+#           Set answer to the correct answer (num1 // num2).
+#           If the response is correct, tell the student and increment the correct response counter.
 #
-#       Display a "Modulus" heading
-#       Begin a loop to cover the 2 modulus questions
-#           Generate 2 random numbers (LOWERBOUND to UPPERBOUND), num1 & num2
-#           Display a modulus question and get a response
-#           Increment the question counter
-#           Compute the correct answer (num1 + num2)
-#           If the response is correct, tell the student and increment the correct response counter
+#       Display a "Modulus" heading.
+#       Begin a loop to cover the 2 modulus questions.
+#           Generate 2 random numbers (LOWERBOUND to UPPERBOUND), num1 & num2.
+#           Display a modulus question and get a response.
+#           Increment the question counter.
+#           Set answer to the correct answer (num1 % num2).
+#           If the response is correct, tell the student and increment the correct response counter.
 #
-#   Display the number of questions answered correctly and the total number of questions
+#   Display the number of questions answered correctly and the total number of questions.
 #
-#   Prompt the user to take the quiz again
-#   if the response is 'y' continue the while-loop
-#   otherwise, break out of the loop and end the program
+#   Prompt the user to take the quiz again.
+#   If the response is 'y' continue the while-loop.
+#   Otherwise, break out of the loop and end the program.
 #
 ############################################################
 
@@ -69,8 +69,8 @@ random.seed()
 LOWERBOUND = 1   # lower bound of the numbers in the questions
 UPPERBOUND = 20  # upper bound of the numbers in the questions
 
-while True: # this is the logical equivalent of a do-while loop
-            # the loop exit logic is at the end of the program
+again = 'y' # set the loop to run the first time
+while again == 'y': # at the end, the user will be asked to try again
 
     # used to keep track of the number of questions given and number answered correctly
     questions, correct = 0, 0
@@ -79,7 +79,7 @@ while True: # this is the logical equivalent of a do-while loop
     print("Test your math skills!  Answer these 10 questions.  Good luck!")
     print()
 
-    print("===== Addition! =====\n")
+    print("===== Addition =====\n")
 
     for rep in range(2):
 
@@ -97,7 +97,7 @@ while True: # this is the logical equivalent of a do-while loop
         print()
 
 
-    print("===== Subtraction! =====\n")
+    print("===== Subtraction =====\n")
 
     for rep in range(2):
 
@@ -118,7 +118,7 @@ while True: # this is the logical equivalent of a do-while loop
         print()
 
 
-    print("===== Multiplication! =====\n")
+    print("===== Multiplication =====\n")
 
     for rep in range(2):
 
@@ -136,7 +136,7 @@ while True: # this is the logical equivalent of a do-while loop
         print()
 
 
-    print("===== Division!  ===== (just the whole number answer)\n")
+    print("===== Division  ===== (just the whole number answer)\n")
 
     for rep in range(2):
 
@@ -157,7 +157,7 @@ while True: # this is the logical equivalent of a do-while loop
         print()
 
 
-    print("===== Modulus!  ===== (the remainder after division)\n")
+    print("===== Modulus  ===== (the remainder after division)\n")
 
     for rep in range(2):
 
@@ -177,12 +177,14 @@ while True: # this is the logical equivalent of a do-while loop
 
         print()
 
-    print("You answered {} out of {} questions correctly!".format(correct, questions))
-    print()
+    endingpunctuation = '!' if correct / questions >= 0.8 else '.'
+    print("You answered {} out of {} questions correctly{}\n"
+          .format(correct, questions, endingpunctuation))
 
     again = input("Do you want to try again? (y/n) ")
-    if again == 'y':
-        continue # continue looping
-    else:
-        print()
-        break    # exit the loop (and end the program)
+    if again == '': # just pressing <Enter> means no
+        again = 'n'
+    again = again[0].lower() # look at just the first character of the response (forced lowercase)
+    # if again == 'y' the outer loop will continue again, otherwise the program ends
+
+print() # print a blank line before the next shell prompt

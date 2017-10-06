3111bdf534afaf0e6dab64005e920df0687f4f7a
Michael Gainey <mikeg9b@gmail.com>
2017-10-06 00:16:05 -0500

    improved variable names; added nice inline comments

--- GaineyM_CSCI1470_HW6.py
+++ GaineyM_CSCI1470_HW6.py
@@ -1,4 +1,4 @@
-############################################################
+###############################################################################
 #
 # Name: Michael Gainey
 #
@@ -9,19 +9,19 @@
 # Algorithm:
 #     blah, blah, blah, ...
 #
-############################################################
+###############################################################################
 
 import turtle
 
-def make_window(backgndColor, windowTitle):
+def make_window(backgroundColor, windowTitle):
     """
       Set up the window with the given background color and title.
       Returns the new window.
     """
-    window = turtle.Screen()
-    window.bgcolor(backgndColor)
-    window.title(windowTitle)
-    return window
+    window = turtle.Screen()        # make a new window
+    window.bgcolor(backgroundColor) # set the background color
+    window.title(windowTitle)       # set the title
+    return window                   # give it back to the calling program
 
 
 def make_turtle(turtleColor, penWidth):
@@ -29,57 +29,65 @@ def make_turtle(turtleColor, penWidth):
       Set up a turtle with the given color and pensize.
       Returns the new turtle.
     """
-    newTurtle = turtle.Turtle()
-    newTurtle.color(turtleColor)
-    newTurtle.pensize(penWidth)
-    return newTurtle
+    newTurtle = turtle.Turtle()  # make a new turtle
+    newTurtle.color(turtleColor) # set the color
+    newTurtle.pensize(penWidth)  # set the pen size
+    return newTurtle             # give it back to the calling program
 
 
-def draw_square(t, sideLength):
-     """Make turtle t draw a square of sideLength."""
-     for i in range(4):
-         t.forward(sideLength)
-         t.left(90)
+def draw_square(theTurtle, sideLength):
+     """Make turtle theTurtle draw a square of sideLength."""
+
+     for side in range(4):
+         theTurtle.forward(sideLength) # draw a side
+         theTurtle.left(90)            # turn left
          
 
-w = make_window("lightgreen", "GaineyM_CSCI1470_HW6")
-t = make_turtle("hotpink", 5)
-t.speed(0)
+window = make_window("lightgreen", "GaineyM_CSCI1470_HW6")
+
+# Introducing Mirtle, the turtle!
+mirtle = make_turtle("hotpink", 5)
+mirtle.speed(0)
 
 # pre-position the turtle so the output will be centered on the screen
-t.penup()
-t.backward(160)
-t.pendown()
+mirtle.penup()
+mirtle.backward(160)
+mirtle.pendown()
 
 # Part 1: Five little squares in a row
+for square in range(5):
+
+    draw_square(mirtle, 20)
+
+    # move to the next square
+    mirtle.penup()
+    mirtle.forward(40)
+    mirtle.pendown()
+
+
+# Separate the output from Part 1 and Part 2
+mirtle.penup()
+mirtle.forward(60)
+mirtle.pendown()
+
+
+# Part 2: Concentric squares
+    
+sideLength = 20
 
 for square in range(5):
-    draw_square(t, 20)
-    t.penup()
-    t.forward(40)
-    t.pendown()
+
+    draw_square(mirtle, sideLength)
+
+    # move (back and down) to the next (larger) square
+    mirtle.penup()
+    mirtle.back(10)
+    mirtle.right(90)
+    mirtle.forward(10)
+    mirtle.left(90)
+    mirtle.pendown()
+
+    sideLength += 20 # the next square will be larger
 
 
-# Separate output from Part 1 and Part 2
-t.penup()
-t.forward(60)
-t.pendown()
-
-
-# Part 2
-    
-side = 20
-for square in range(5):
-    draw_square(t, side)
-    
-    t.penup()
-    t.back(10)
-    t.right(90)
-    t.forward(10)
-    t.left(90)
-    t.pendown()
-
-    side += 20
-
-    
-w.mainloop()
+window.mainloop() # so the window doesn't disappear

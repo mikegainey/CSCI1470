57538d79cff3178dea5a3651c0aeae47aff866b1
Michael Gainey <mikeg9b@gmail.com>
2017-10-12 22:20:17 -0500

    string-multiply function while I should have been studying physics

---
+++ string-mutliply.py
@@ -0,0 +1,51 @@
+# input = [1 (2 (5)*3 3)*2 7], output = 1 2 5 5 5 3 2 5 5 5 3 7
+
+# Note: I didn't know the rules of how to form the possible inputs.  So, this function may sometimes fail because
+# my method of finding the matching left parenthesis is very naive.  But it works with the example you gave me in class.
+
+# strmult = string multiply; my name for what this program does
+# inpstr = input string
+# opx = operator (*) index
+# argx = argument index; this is the number to multiply by; in '[1 (2 (5)*3 3)*2 7]' the arguments are 3 and 2
+# arg = the argument (argx is the index of the argument)
+# rparenx = right parenthesis index (just to the left of the operator)
+# lparenx = left parenthesis index; searches left from the right parenthasis; this is not an intelligent way of finding
+#   the correct matching parenthesis; it might fail on other inputs -- I don't know all the rules of what inputs can be
+# insert1 = the string to be multiplied; in [(5)*3], insert = 5; in [1 (2 5 5 5 3)*2 7], insert = 2 5 5 5 3
+# insert2 = if insert1 = 5 and arg = 3, then insert2 = ['5', '5', '5']
+# insert3 = if insert2 = ['5', '5', '5'], then insert3 = '5 5 5'
+# leftstr = the left part of the string that's not being multiplied
+# rightstr = the right part of the string that's not being multiplied
+
+
+def strmult(inpstr):
+
+    while '*' in inpstr: # run the body of this function for each '*' (left to right)
+
+        opx = inpstr.find('*')  # the index of the *
+        argx = opx + 1          # the index of the number to multiply by
+        arg = int(inpstr[argx]) # the number to multiply by
+        rparenx = opx - 1       # the index of the right paren (just left of the *)
+        lparenx = inpstr.rfind('(', 0, rparenx) # the index of the left paren (might find the wrong one)
+
+        insert = inpstr[(lparenx + 1):rparenx]  # the string to be multiplied (within parens but not including them)
+        insert = [insert for a in range(arg)]   # do the multiplication with a list comprehension
+        insert = ' '.join(insert)               # convert to a string with spaces in between
+
+        leftstr = inpstr[:lparenx]              # the left part of the rest of the string
+        rightstr = inpstr[(argx + 1):]          # the right part of the rest of the string
+        inpstr = leftstr + insert + rightstr    # put it all together
+
+    return inpstr[1:-1] # and strip the square brackets off
+
+
+s = '[(2)*3]'
+print("\ninput = {}, output = {}".format(s, strmult(s)))
+
+s = '[1 (2 (5)*3 3)*2 7]' # output should be: 1 2 5 5 5 3 2 5 5 5 3 7
+print("\ninput = {}, output = {}".format(s, strmult(s)))
+
+s = '[1 (2 5 5 5 3)*2 7]' # output should be: 1 2 5 5 5 3 2 5 5 5 3 7
+print("\ninput = {}, output = {}".format(s, strmult(s)))
+
+print()

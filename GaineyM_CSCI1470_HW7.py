77628984361a12600211d0a01328b506afda06cf
Michael Gainey <mikeg9b@gmail.com>
2017-10-14 01:45:08 -0500

    improved variable names and inline comments
    - still need to write preface comments

--- GaineyM_CSCI1470_HW7.py
+++ GaineyM_CSCI1470_HW7.py
@@ -11,42 +11,43 @@
 #
 ###############################################################################
 
-def translate(phnum):
-    '''Translate an alphanumeric phone number to an all-numeric phone number'''
-    output = ''      # initialize the output string
-    for ch in phnum: # build the output string
-        if ch in 'ABCabc':
-            output += '2'
-        elif ch in 'DEFdef':
-            output += '3'
-        elif ch in 'GHIghi':
-            output += '4'
-        elif ch in 'JKLjkl':
-            output += '5'
-        elif ch in 'MNOmno':
-            output += '6'
-        elif ch in 'PQRSpqrs':
-            output += '7'
-        elif ch in 'TUVtuv':
-            output += '8'
-        elif ch in 'WXYZwxyz':
-            output += '9'
-        elif ch in '0123456789': # if the current character is a number, juss pass it through
-            output += ch
-    first3 = output[:3]
-    second3 = output[3:6]
-    last4 = output[-4:]
-    output = '-'.join([first3, second3, last4])
-    return output # return the output string
+def translate(alphanumeric):
+    '''Given an alphanumeric phone number, return the equivalent all-numeric phone number.
+       translate(alphanumeric: str) -> str
+    '''
+
+    numeric = ''
+    for character in alphanumeric: # traverse the input
+        if character in 'ABCabc':  # build the output string
+            numeric += '2'
+        elif character in 'DEFdef':
+            numeric += '3'
+        elif character in 'GHIghi':
+            numeric += '4'
+        elif character in 'JKLjkl':
+            numeric += '5'
+        elif character in 'MNOmno':
+            numeric += '6'
+        elif character in 'PQRSpqrs':
+            numeric += '7'
+        elif character in 'TUVtuv':
+            numeric += '8'
+        elif character in 'WXYZwxyz':
+            numeric += '9'
+        else:
+            numeric += character # just pass through any other character
+
+    return numeric
 
 
-while True:
-    phonein = input("\nEnter a 10 character alphanumeric telephone number in the form of XXX-XXX-XXXX: ")
-    print("The equivalent numeric phone number is {}".format(translate(phonein)))
+again = 'y'         # so the loop will run the first time
+while again == 'y': # loop until the user decides to stop
+
+    phoneNumber = input("\nEnter a 10 character alphanumeric telephone number in the form of XXX-XXX-XXXX: ")
+    print("The equivalent numeric phone number is {}".format(translate(phoneNumber)))
+
     again = input("Would you like to translate another number? (y/N) ")
-    again = again or 'n'
-    again = again[0].lower()
-    if again != 'y':
-        break
-    else:
-        continue
+    again = again or 'n'     # just pressing <Enter> means no
+    again = again[0].lower() # look at only the first character and convert to lowercase
+
+print()

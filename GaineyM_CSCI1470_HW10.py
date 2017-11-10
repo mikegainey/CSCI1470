4a4db18151058771fae6eccb878f0101df780cd4
Michael Gainey <mikeg9b@gmail.com>
2017-11-09 18:59:36 -0600

    Misunderstood the rules! need to correct

--- GaineyM_CSCI1470_HW10.py
+++ GaineyM_CSCI1470_HW10.py
@@ -1,3 +1,6 @@
+# Note: I misunderstood the rules of the game.  For this implementation, whoever
+# gets over 21 first wins.
+
 ###############################################################################
 #
 # Name: Michael Gainey
@@ -7,7 +10,46 @@
 # Assignment: Homework #10
 #
 # Pseudocode:
-#   Blah, blah, blah, ...
+#   Import the random module from the Python Standard Library to generate random numbers
+#   Seed the random number generator
+#
+#   Set player1 by prompting player 1 for his/her name.
+#   Set player2 by prompting player 2 for his/her name.
+#
+#   Initialize list player to ['placeholder', player1, player2]
+#   Initialize list score to a list of three zeros
+#
+#   Set currentPlayer to a random number between 1 and 2
+#   Tell the users which player will go first: player[currentPlayer]
+#
+#   Begin the main game loop:
+#
+#       Set roll to a list of three random numbers from one to six
+#       Display the currentPlayer's roll
+#
+#       If the roll is 6 6 6 ...
+#           Set currentPlayer's score to 21
+#           Display a message: "Bunko! ..."
+#
+#       else if the roll contains two 6's:
+#           Add 5 to currentPlayer's score
+#           Display a message: "+5 points ..."
+#
+#       else if the roll contains one 6:
+#           Add 1 to the currentPlayer's score
+#           Display a message: "+1 point ..."
+#
+#       else ...
+#           Toggle currentPlayer between 1 and 2
+#           Display a message: "next player's turn ..."
+#
+#       Display the two scores: score[1] and score[2]
+#
+#       Set higher to the higher of the two scores
+#       If higher is greater than or equal to 21, then exit the loop
+#
+#   Set winner to the index of higher in score
+#   Display the winner
 #
 ###############################################################################
 
@@ -30,11 +72,11 @@ print("\n{} will go first.\n".format(player[currentPlayer]))
 while True:
 
     # roll is a list representing the rolling of three dice
-    roll = [random.randint(5,6) for r in range(3)]
+    roll = [random.randint(1,6) for r in range(3)]
     print("{} rolls a {} {} {}".format(player[currentPlayer], roll[0], roll[1], roll[2]), end='    ')
     
     if roll.count(6) == 3:
-        score[currentPlayer] = 21
+        score[currentPlayer] += 21
         print("Bunko for {}!!!".format(player[currentPlayer]))
 
     elif roll.count(6) == 2:
